8b22ea13721e907959ddfcf1d81b928027dab4c2
marwan darwish <darawish663@gmail.com>
2021-07-13 20:34:33 +0200

    category

--- gift/src/index.js
+++ gift/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import '../node_modules/jquery/dist/jquery.min.js'
+import '../node_modules/popper.js/dist/umd/popper.min.js'
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 
 ReactDOM.render(
   <React.StrictMode>

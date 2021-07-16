591cdc5c788fadf4d3948d3a0c4dfaa68f1df205
abdelrahmanes <abdelrahmanelshershaby@gmail.com>
2021-07-16 22:40:28 +0200

    adding product component

--- gift/src/App.js
+++ gift/src/App.js
@@ -6,12 +6,13 @@ import "popper.js/dist/umd/popper.min";
 import "bootstrap/dist/js/bootstrap.min";
 import ContactPage from './component/ahmed/contactpage/ContactPage';
 import ShoppingCart from './component/ahmed/shoppingcart/ShoppingCart';
-
+import Product from './components/Product/Product';
 
 import Category from './components/category/category';
 function App() {
   return (<>
     <Category />
+    <Product/>
     <ShoppingCart />
     <ContactPage />
   </>
